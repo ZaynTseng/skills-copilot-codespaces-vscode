c22449ebfc4dc445bad32dcbe7b8689283bfd16e
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-07-18 14:32:35 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+// Context from Code Snippet .github/workflows/4-copilot-comment.yml
+// name: On code generated from comment
+//
+// # We will only run this action when:
+// # 1. This repository isn't the template repository.
+// # Reference: https://docs.github.com/en/actions/learn-github-actions/contexts
+// # Reference: https://docs.github.com/en/actions/learn-github-actions/expressions
+// on_push_to_comments:
+//   name: On code generated from comment
+//   needs: get_current_step
+//
